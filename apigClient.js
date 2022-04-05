4b426d6233f22c68ad99290af45de18bb0c0fa92
kingdevtk <61067769+kingdevtk@users.noreply.github.com>
2022-04-05 11:00:10 -0400

    Update apigClient.js

--- apigClient.js
+++ apigClient.js
@@ -1,1 +1,45 @@
+var apigClientFactory = require('aws-api-gateway-client').default;
+config = {invokeUrl:'https://g21z1pb3pa.execute-api.us-east-1.amazonaws.com/prod'}
+var apigClient = apigClientFactory.newClient(config);
+var params = {
+  // This is where any modeled request parameters should be added.
+  // The key is the parameter name, as it is defined in the API in API Gateway.
+  param0: '',
+  param1: ''
+};
 
+var body = {
+  // This is where you define the body of the request,
+};
+
+var additionalParams = {
+  // If there are any unmodeled query parameters or headers that must be
+  //   sent with the request, add them here.
+  headers: {
+    param0: '',
+    param1: ''
+  },
+  queryParams: {
+    param0: '',
+    param1: ''
+  }
+};
+
+apigClient.GET(params, body, additionalParams)
+    .then(function(result){
+      // Add success callback code here.
+    }).catch( function(result){
+      // Add error callback code here.
+  
+apigClient.OPTIONS(params, body, additionalParams)
+    .then(function(result){
+      // Add success callback code here.
+    }).catch( function(result){
+      // Add error callback code here.    
+  
+apigClient.POST(params, body, additionalParams)
+    .then(function(result){
+      // Add success callback code here.
+    }).catch( function(result){
+      // Add error callback code here.  
+ });
