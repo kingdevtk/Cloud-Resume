c9b24f4dbc7eb6df8c0d483b013043dc010cd5c0
kingdevtk <61067769+kingdevtk@users.noreply.github.com>
2022-04-06 12:20:28 -0400

    Update counter.js

--- counter.js
+++ counter.js
@@ -1,1 +1,16 @@
+var url = "https://g21z1pb3pa.execute-api.us-east-1.amazonaws.com/prod";
+
+var xhr = new XMLHttpRequest();
+xhr.open("GET", url);
+
+var xhr = new XMLHttpRequest();
+xhr.open("POST", url);
+
+xhr.onreadystatechange = function () {
+   if (xhr.readyState === 4) {
+      console.log(xhr.status);
+      console.log(xhr.responseText);
+   }};
+
+xhr.send();
 
