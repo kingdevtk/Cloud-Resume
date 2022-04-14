a55eb844eb58ec2301e40fa6c3694717bf06ef58
kingdevtk <61067769+kingdevtk@users.noreply.github.com>
2022-04-14 11:08:17 -0400

    Update counter.js

--- counter.js
+++ counter.js
@@ -1,17 +1,17 @@
-<script>
-    function getLoadCount(){
+ function getLoadCount(){
 
-        // call an AWS gateway API, that then calls an AWS Lambda
+        // call an AWS gateway API, that then calls an AWS Lambda function
         // to read and then update AWS DynamoDB item showing load count
 
+       
         var req = new XMLHttpRequest();  
         req.open('GET', 
-        "https://g21z1pb3pa.execute-api.us-east-1.amazonaws.com/prod", 
+        "https://cimk9zx0ch.execute-api.ap-southeast-2.amazonaws.com/beta", 
         false);   
         req.send();  
         if(req.status == 200)
             {  
-                len = req.responseText.length;
+               var len = req.responseText.length;
                 // response text has opening and closing double quotes
                 console.log(req.responseText.substring(1, len-1));
                 // set field in footer
@@ -20,18 +20,14 @@
             }
         else {
             // NOT OK response, so log in console
-            console.log(req.status)
+            console.log(req.status);
         }//end if
     }// end getLoadCount
 
-    // read and write visit count into footer, then increment count in DB
+    // read and write visit count into page, then increment count in DB
     window.onload = function(){
 
             // side effects
-            // getLoadCount() updates field in footer, increments count in DB
+            // getLoadCount() updates field on page, increments count in DB
             getLoadCount();
         }; //end window.onload
-
-
-
-</script>
