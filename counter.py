bdc5aa5a87d44583f871fa4c2ca18cd8f1e131de
kingdevtk <61067769+kingdevtk@users.noreply.github.com>
2022-04-14 16:13:35 -0400

    Update counter.py

--- counter.py
+++ counter.py
@@ -1,3 +1,8 @@
 import requests
+
 response = requests.get('https://g21z1pb3pa.execute-api.us-east-1.amazonaws.com/prod')
+response = requests.post('https://g21z1pb3pa.execute-api.us-east-1.amazonaws.com/prod')
 print(response.json())
+if response.status_code == 404:
+    print("Result not found!")
+    # Code here will react to failed requests
