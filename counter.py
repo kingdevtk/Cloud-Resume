ab2be5d9c4d277b0c03340ce33b657f4c20bce7e
kingdevtk <61067769+kingdevtk@users.noreply.github.com>
2022-04-15 08:23:40 -0400

    Update counter.py

--- counter.py
+++ counter.py
@@ -1,6 +1,9 @@
+type="module"
 import requests
 
 response = requests.get('https://g21z1pb3pa.execute-api.us-east-1.amazonaws.com/prod')
+response.status_code
+200
 response = requests.post('https://g21z1pb3pa.execute-api.us-east-1.amazonaws.com/prod')
 print(response.json())
 if response.status_code == 404:
